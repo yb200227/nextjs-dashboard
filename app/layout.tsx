eb9ccf46df9e34525b0e011ebac71e149d0f4ef2
yb200227 <yb200227@gmail.com>
2024-11-10 16:57:01 -0500

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
-}
 }
